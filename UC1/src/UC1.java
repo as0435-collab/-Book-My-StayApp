aaa826e96cf1f5fffefcf70d2c97279b56f9589c
as0435-collab <as0435@srmist.edu.in>
2026-04-09 15:39:48 +0530

    commit message

---
+++ UC1/src/UC1.java
@@ -0,0 +1,26 @@
+public class UC1 {
+    /**
+     * UseCase1HotelBookingApp
+     *
+     * This class represents the entry point of the Book My Stay application.
+     * It demonstrates how a Java program starts execution using the main()
+     * method and displays a welcome message with application details.
+     *
+     * @author Ahmed
+     * @version 1.0
+     */
+        /**
+         * Main method - Entry point of the application
+         *
+         * @param args Command line arguments
+         */
+        public static void main(String[] args) {
+
+            System.out.println("=======================================");
+            System.out.println("     Welcome to Book My Stay App       ");
+            System.out.println("=======================================");
+            System.out.println("Application Name : Hotel Booking Management System");
+            System.out.println("Version          : v1.0");
+            System.out.println("System started successfully.");
+        }
+    }
